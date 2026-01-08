e86c99b586856194983e38b69867afdbbd55c400
Khalid Abdi <xamza2009@gmail.com>
2026-01-08 17:36:03 +0300

    fix params error 2

--- components/otp-form.tsx
+++ components/otp-form.tsx
@@ -19,8 +19,7 @@ import { useSearchParams } from "next/navigation"
 import { useState } from "react"
 import { authClient } from "@/lib/auth-client"
 import { useRouter } from "next/navigation"
-import { da } from "zod/locales"
-
+import { Suspense } from "react"
 export function OTPForm({ className, ...props }: React.ComponentProps<"div">) {
   const router =useRouter()
   const[otp,SetOtp]=useState<string>("")
@@ -50,6 +49,7 @@ console.log("sign up successfuly :",data)
 
 
   return (
+    <Suspense>
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <form className="flex justify-center items-center " onSubmit={check}>
         <FieldGroup>
@@ -137,5 +137,6 @@ console.log("sign up successfuly :",data)
         and <a href="#">Privacy Policy</a>.
       </FieldDescription>
     </div>
+    </Suspense>
   )
 }

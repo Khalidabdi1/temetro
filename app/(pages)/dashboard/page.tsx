c1a45ce553b9f0f8ccb65aa840d0f40df3cc0172
Khalid Abdi <xamza2009@gmail.com>
2026-01-09 01:34:37 +0300

    add sidebar ui

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar02 from '@/components/sidebar-02'
 
 const page = () => {
   return (
     <div>
-      welcome in dashboard
+    <Sidebar02/>
     </div>
   )
 }
